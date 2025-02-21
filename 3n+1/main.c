dad78b3a3430066b16c336ca6ddfe0471e411a31
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-02-21 16:05:29 +0800

    CPE

---
+++ 3n+1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int start = 0, end = 0, step = 0, stepmax = 0;
+    while(scanf("%d%d", &start, &end) != EOF){
+        printf("%d %d ", start, end);
+        if(end < start){
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+        for(int i = start; i <= end; i++){
+            int n = i;
+            while(n != 1){
+                if(n % 2 != 0){
+                    n = 3 * n + 1;
+                }else if(n % 2 == 0){
+                    n = n / 2;
+                }
+                step++;
+            }
+            if(stepmax < step++){
+                stepmax = step;
+            }
+            step = 0;
+        }
+        printf("%d\n", stepmax);
+        stepmax = 0;
+    }
+}
