bb23cb5a7c83cedcf6a133b9797b0d424af881f4
GarryAdam <one030914@gmail.com>
2025-04-17 23:08:26 +0800

    CPE

--- Tree Recovery (uva536)/main.py
+++ Tree Recovery (uva536)/main.py
@@ -1,40 +1,18 @@
-def bfs(graph, start, end):
-    queue = [start]
-    visited = {start: None}
+def find_path(preO, inO):
+    if len(preO) == 0:
+        return ""
+    root = preO[0]
+    root_index = inO.index(root)
+    left_subtree = find_path(preO[1:root_index + 1], inO[:root_index])
+    right_subtree = find_path(preO[root_index + 1:], inO[root_index + 1:])
+    return left_subtree + right_subtree + root
 
-    while queue:
-        curr = queue.pop(0)
-        if curr == end:
-            break
-        for neighbor in graph.get(curr, []):
-            if neighbor not in visited:
-                visited[neighbor] = curr
-                queue.append(neighbor)
-
-    path = []
-    while end is not None:
-        path.append(end)
-        end = visited.get(end)
-    path.reverse()
-    return ''.join(city[0] for city in path)
-
-t = int(input())
-for test in range(t):
-    space = input()
-        
-    n, q = map(int, input().split())
-    graph = {}
-
-    for _ in range(n):
-        a, b = input().split()
-        if a not in graph:
-            graph[a] = []
-        if b not in graph:
-            graph[b] = []
-        graph[a].append(b)
-        graph[b].append(a)
-
-    for _ in range(q):
-        a, b = input().split()
-        print(bfs(graph, a, b))
-
+while True:
+    try:
+        preO, inO = input().split()
+        preO = list(preO)
+        inO = list(inO)
+        result = find_path(preO, inO)
+        print(result)
+    except EOFError:
+        break
